21c204b844ce4a2f683668673c3cb7be3d04ce0d
Ramdani <xhijack@gmail.com>
2025-08-31 17:31:36 +0700

    add history tiang

--- tekma_app/utils.py
+++ tekma_app/utils.py
@@ -28,10 +28,35 @@ def create_stock_entry(doc, is_return=False):
     se.submit()
     frappe.msgprint("Tiang dikeluarkan")
 
+def create_stock_entry_issued(doc):
+    se = frappe.new_doc("Stock Entry")
+    se.purpose = "Material Issue"
+    se.from_bom = 0
+    se.company = doc.company
+    se.posting_date = doc.posting_date
+    se.posting_time = doc.posting_time
+    se.stock_entry_type = "Material Issue"
+    se.doc_reference = doc.name
+    se.items = []
+    for item in doc.items:
+        se.append("items", {
+            "item_code": "Tiang",
+            "item_name": item.item_name,
+            "item_group": item.item_group,
+            "qty": item.qty,
+            "s_warehouse": "Stores - MK"
+        })
+    if se.items:
+        se.insert()
+        se.submit()
+        frappe.msgprint("Tiang dikeluarkan")
+
 def move_tiang(doc):
     for item in doc.items:
-        if item.tiang == "Dengan Tiang":
+        if item.tiang == "Tukar Tiang":
             create_stock_entry(doc, doc.is_return)
+        elif item.tiang == "Dengan Tiang":
+            create_stock_entry_issued(doc)
 
 def cancel_stock_entry(doc):
     se = frappe.get_all("Stock Entry", filters={"doc_reference": doc.name}, fields=["name"])
@@ -39,19 +64,46 @@ def cancel_stock_entry(doc):
         stock_entry = frappe.get_doc("Stock Entry", s.name)
         stock_entry.cancel()
 
+def log_history_tiang(doc):
+    ht = frappe.new_doc("History Tiang")
+    ht.customer = doc.customer
+    ht.date = doc.posting_date
+    ht.document_type = doc.doctype
+    ht.document = doc.name
+    ht.qty = sum([item.qty for item in doc.items if item.tiang == "Dengan Tiang"])
+    ht.insert()
+    ht.submit()
+
+def cancel_log_history_tiang(doc):
+    ht_names = frappe.get_all("History Tiang",
+        filters={"document": doc.name, "docstatus": 1},
+        pluck="name"
+    )
+    for nm in ht_names:
+        ht = frappe.get_doc("History Tiang", nm)
+        ht.flags.ignore_permissions = True
+        ht.cancel()
+    doc.flags.ignore_links = True
+
 def delivery_note_on_submit(doc, method):
     move_tiang(doc)
+    log_history_tiang(doc)
 
 def delivery_note_on_cancel(doc, method):
-    cancel_stock_entry(doc) 
+    cancel_stock_entry(doc)
+    cancel_log_history_tiang(doc)
+    # cancel_linked_history_tiang(doc)
 
 def sales_invoice_on_submit(doc, method):
     if doc.update_stock:
         move_tiang(doc)
+        
 
 def sales_invoice_on_cancel(doc, method):
     if doc.update_stock:
         cancel_stock_entry(doc)
+        cancel_log_history_tiang(doc)
+
 
 def validate_ratio_for_valuation_rate_stock_entry(doc):
     total_ratio = 0
